d35ee9120cd662a6d4504964a49a797cb9ebf992
David Marinov <73773220+DavidMarinov22@users.noreply.github.com>
2025-05-12 23:35:16 +0100

    Fixing errors

--- rm_sys/staff/models.py
+++ rm_sys/staff/models.py
@@ -15,3 +15,24 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class InventoryItem(models.Model):
+    
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, blank=True)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=200, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    def needs_reorder(self):
+        return self.quantity <= self.reorder_level
